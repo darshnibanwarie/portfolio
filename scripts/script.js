1b2c55d9289e14106adca51ee91872a90500a3a6
darshnibanwarie <43376524+darshnibanwarie@users.noreply.github.com>
2021-11-23 00:05:55 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+
+/* menu knop animation */
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
+
